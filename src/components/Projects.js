f0bb930b678dbd6e82a83869146a642b4e5f815c
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-07-28 16:11:04 -0700

    Added Sosh to projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,34 @@
 import React from "react";
+import soshPreview from "../resources/soshPreview.png";
 
 export default function Projects() {
   return (
     <div id="projects">
       <div className="sectionHeading">Projects</div>
+      <div className="soshProject">
+        <div className="projectTextContainer">
+          <div className="projectPreviewText">
+            <div className="projectTitle">Sosh</div>
+            <div className="projectSubheading">
+              A full-stack social-media platform.
+            </div>
+            <p className="projectInfo">
+              Modern functionality built with React, Node, Express, MongoDB,
+              Framer-Motion, React-Flip-Toolkit, and AWS S3.
+            </p>
+          </div>
+          <div></div>
+          <p className="projectLink">Live App</p>
+          <p className="projectLink">Learn More</p>
+        </div>
+        <div className="projectContainer">
+          <img
+            className="projectPreview"
+            src={soshPreview}
+            alt="Sosh App Preview"
+          ></img>
+        </div>
+      </div>
     </div>
   );
 }
