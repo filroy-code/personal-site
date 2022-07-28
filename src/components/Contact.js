714cceb634c4f2c55310c6fa51f5c40ddc8b050a
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-07-28 14:32:11 -0700

    Contact section completed.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,18 @@
 import React from "react";
+import emailIcon from "../resources/email.png";
 
 export default function Contact() {
   return (
     <div id="contact">
       <div className="sectionHeading">Contact</div>
+      <div className="contactParagraph">
+        Want to collaberate? Questions about my journey? Have an opportunity
+        you'd like to discuss? Contact me at the email below.
+      </div>
+      <div className="contactBox">
+        <img className="emailIcon" src={emailIcon} alt="Email Address"></img>
+        <div>filip_andrejuk@hotmail.com</div>
+      </div>
     </div>
   );
 }
