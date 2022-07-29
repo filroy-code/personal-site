8126d681f08d20be5d369ad82d1c988b26a1feb3
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-07-29 14:01:43 -0700

    Footer completed.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,17 @@
 import React from "react";
+import githubIcon from "../resources/github.png";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <a
+        href="https://www.filroy-code.github.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="footerIcon" src={githubIcon} alt="Github"></img>
+      </a>
+      <div>Filip Andrejuk - © 2022</div>
+    </footer>
+  );
 }
