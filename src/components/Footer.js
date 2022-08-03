d06105097e0487a1f9073b4f3c1d0a996e221ddc
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-08-03 12:12:04 -0700

    Responsiveness improvements in project section. Updated metadata in index.html.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,7 @@ import githubIcon from "../resources/github.png";
 export default function Footer() {
   return (
     <footer>
-      <a
-        href="https://www.filroy-code.github.com"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href="https://github.com/filroy-code" target="_blank" rel="noreferrer">
         <img className="footerIcon" src={githubIcon} alt="Github"></img>
       </a>
       <div>Filip Andrejuk - © 2022</div>
