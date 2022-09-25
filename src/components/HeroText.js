ee57eb6b60a31f50407facef9290accb7efe2915
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-09-25 19:38:31 -0400

    Improved hero text animations on page load. Text now moves in from the side instead of fading in and is sequenced with setTimeout.

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -2,17 +2,40 @@ import React from "react";
 
 export default function HeroText(props) {
   const [loading, setLoading] = React.useState(true);
+  const goToContentRef = React.useRef(null);
+  const secondHeadingRef = React.useRef(null);
+
+  if (!loading) {
+    setTimeout(() => {
+      secondHeadingRef.current.classList.remove("heroHeadingLoading");
+      secondHeadingRef.current.classList.add("heroHeading");
+    }, 1000);
+  }
+
+  if (!loading) {
+    setTimeout(() => {
+      goToContentRef.current.classList.add("visible");
+    }, 2000);
+  }
 
   React.useEffect(() => setLoading(false), []);
   return (
     <div className="heroText">
-      <div className={loading ? "heroHeadingLoading" : "heroHeading"}>
+      <div
+        className={loading ? "heroHeadingLoading" : "heroHeading"}
+        style={{ position: "relative" }}
+      >
         Hi, I'm <span style={{ color: "rgb(236, 76, 76)" }}>Fil.</span>
       </div>
-      <div className={loading ? "heroHeadingLoading" : "heroHeading"}>
+      <div
+        style={{ position: "relative" }}
+        ref={secondHeadingRef}
+        className={"heroHeadingLoading"}
+      >
         I'm a full stack web developer.
       </div>
       <div
+        ref={goToContentRef}
         onClick={() => {
           props.projectsLink.current.scrollIntoView({
             behavior: "smooth",
